c4183d900169cf87f2a94efb32d63e486628cf64
Nigel P Richardson <nigelprichardson@sky.com>
2020-12-03 10:42:39 +0000

    Loading Chapter Info

---
+++ Chapter1/main.js
@@ -0,0 +1,40 @@
+(function()
+{
+    'use strict'
+    // your code will start here
+
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    // var c = Shape.Circle(200,200,50);
+    // c.fillColor='green';
+
+     var c = Shape.Circle(200, 200, 80);
+     c.fillColor = 'black';
+     var text = new PointText(200, 200);
+     text.justification = 'center';
+     text.fillColor = 'white';
+     text.fontSize = 20;
+     text.content = 'hello world!';
+     
+     for (var x = 25; x < 400; x+= 50)
+     {
+         for (var y = 25; y < 400; y+= 50)
+         {
+            var tool = new Tool();
+            tool.onMouseDown = function(event)
+            {
+                var c = Shape.Circle(event.point.x, event.point.y, 20);
+                c.fillColor = 'green';
+            }
+    //         c = Shape.Circle(x, y, 20);
+    //         c.fillColor = 'yellow';
+         }
+     }
+
+
+
+    paper.view.draw();
+
+    // and end here
+}())
