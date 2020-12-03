a19d824f19398dcd8ce82e1562129d1ab2744288
Nigel P Richardson <nigelprichardson@sky.com>
2020-12-03 18:04:53 +0000

    added eslint

--- lj/Chapter2/demo.js
+++ lj/Chapter2/demo.js
@@ -4,20 +4,7 @@ const _ = require('underscore');
 const sentences = [{subject: 'JavaScript', verb: 'is', object: 'great'},
                    {subject: 'Elephants', verb:'are', object:'large'}];
 
-// ES6 feature: object destructuring
-function say({subject, verb, object})
-{
-    // es6 feature: template strings
-    // note the quotes below a re (`), and not single quotes (')
-console.log(`${subject}, ${verb}, ${object}`);
-}
-// es6 feature for .. of
-//for (let s of sentences)
-//{
-//    say(s);
-//}
-
-const verbs = _.pluck(sentences, 'verb')
+const verbs = _.pluck(sentences, 'verb');
 
 for (let v of verbs)
 {
