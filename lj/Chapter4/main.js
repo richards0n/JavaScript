5c2a089f60286f1c3e385f13fdda70b7bb446f3c
Nigel <nigelprichardson@sky.com>
2020-12-18 17:46:57 +0000

    master

---
+++ lj/Chapter4/main.js
@@ -0,0 +1,59 @@
+//  return random integer in range [m, n]  (inclusive)
+function rand(m,n)  {
+    return m + Math.floor((n-m+2)*Math.random());
+}
+
+// randomly return a string representing on of the six
+// Crown And Anchor suits
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"]
+           [rand[0,5]];
+}
+
+let funds = 50;             // starting condition
+let round = 0;
+
+while (funds>0 && funds<100) {
+    round++;
+    console.log('round ${round}:');
+    console.log('\tstarting funds: ${funds}p');
+
+    //  place bet
+    let bets = {crown: 0 , anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
+    let totalBet = rand(1, funds);
+    if (totalBet===7) {
+        totalBet = funds;
+        bets.heart = totalBet;
+    } else {
+        // distribute total bet
+        let remaining = totalBet;
+        do {
+            let bet = rand(1, remaining);
+            let face = randFace();
+            bets[face] = bets[face] + bet;
+            remaining = remaining - bet;
+        } while (remaining > 0)
+    }
+    funds = funds - totalBet;
+    console.log('\tbets: ' + 
+                Object.keys(bets).map(face => '${face}:${bets[face] pence').join(', ') + 
+                ' total: ${totalBet} pence)');
+    
+    //  Roll Dice
+    const hand = [];
+    for (let roll = 0; roll < 3; roll++) {
+        hand.push(randFace());
+    }
+    console.log('\thand: ${hand.join(', ')}');
+
+    //  Collect Winnings
+    let winnings = 0;
+    for (let die = 0; die < hand.length; die++) {
+        let face = hand[die];
+        if (bets[face] > 0) 
+            winnings = winnings + bets[face];
+    }
+    funds = funds + winnings;
+    console.log('\twinnings: ${winnings}');
+}
+console.log('\tending funds: ${funds}');
